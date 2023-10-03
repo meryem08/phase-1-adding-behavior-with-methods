e277d4ac34c677956adf372853883043a4579966
meryem08 <meriem.mansouri08@gmail.com>
2023-10-03 08:03:37 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  
+  const touti = new Mouse('touti', 'male');
+  const loula = new Mouse('loula', 'female');
+  
+  console.log(mickey.speak()); 
+  console.log(minnie.speak()); 
+  
